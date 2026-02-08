43a7fd0367d000496f2f3980e13253f20e0433d3
alex <ssokoloss1@gmail.com>
2026-02-08 16:16:26 +0300

    added sum.sh

---
+++ sum.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# != 2 ]
+then
+  echo "The number of arguments must be equal to 2."
+  exit 1
+fi
+
+if ! echo "$1" | grep -qE '^-?[0-9]+(\.[0-9]+)?$'
+then
+  echo "First argument must be number."
+  exit 1
+fi
+
+if ! echo "$2" | grep -qE '^-?[0-9]+(\.[0-9]+)?$'
+then
+  echo "Second argument must be number."
+  exit 1
+fi
+
+
+sum=$(echo "$1 + $2" | bc)
+echo "$sum"
+
