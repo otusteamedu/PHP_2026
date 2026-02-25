7648a88ee4d1c387bf0b75a6126593494126890f
Albert <haralb01@gmail.com>
2026-02-25 23:18:34 +0400

    FIX: Cr -> CRLF

--- src/Domain/Email/ValidationResult.php
+++ src/Domain/Email/ValidationResult.php
@@ -1,1 +1,14 @@
-<?phpdeclare(strict_types=1);namespace Aharutyunyan\Hw\Domain\Email;final class ValidationResult{    public function __construct(        public bool $isValid,        public string $reason = '',        public array $details = []    ) {}}
+<?php
+
+declare(strict_types=1);
+
+namespace Aharutyunyan\Hw\Domain\Email;
+
+final class ValidationResult
+{
+    public function __construct(
+        public bool $isValid,
+        public string $reason = '',
+        public array $details = []
+    ) {}
+}
