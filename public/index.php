f31ec3a03fae061f8c7c34c834f0f1c1c6ab9b4a
Albert <haralb01@gmail.com>
2026-03-03 00:42:25 +0400

    FIX: Clear index php file

--- public/index.php
+++ public/index.php
@@ -4,28 +4,29 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Aharutyunyan\Hw\Infrastructure\Validator\CompositeValidator;
-use Aharutyunyan\Hw\Infrastructure\Validator\Strategy\SyntaxValidator;
-use Aharutyunyan\Hw\Infrastructure\Validator\Strategy\DomainValidator;
-use Aharutyunyan\Hw\Infrastructure\Validator\Strategy\DisposableEmailValidator;
-use Aharutyunyan\Hw\Domain\Email\ValueObject\Email;
-
-$composite = new CompositeValidator();
-
-$composite->add(
-    new SyntaxValidator()
-);
-
-$composite->add(
-    new DomainValidator()
-);
-
-$composite->add(
-    new DisposableEmailValidator()
-);
-
-$result = $composite->validate(new Email('test@mailinator.com'));
+$handler = new \Aharutyunyan\Hw\Interfaces\Handler\EmailValidationHandler();
+$result = $handler->handle();
 
 echo '<pre>';
 print_r($result);
 echo '</pre>';
+
+//$composite = new CompositeValidator();
+//
+//$composite->add(
+//    new SyntaxValidator()
+//);
+//
+//$composite->add(
+//    new DomainValidator()
+//);
+//
+//$composite->add(
+//    new DisposableEmailValidator()
+//);
+//
+//$result = $composite->validate(new Email('test@mailinator.com'));
+//
+//echo '<pre>';
+//print_r($result);
+//echo '</pre>';
