e696c62a3d945aed23116e48a0c3474839c3b443
alex <ssokoloss1@gmail.com>
2026-03-19 01:18:42 +0300

    added another way to do

--- leetCode.php
+++ leetCode.php
@@ -4,37 +4,29 @@ declare(strict_types=1);
 
 class Solution {
 
-    function mergeTwoLists(?ListNode $list1, ?ListNode $list2): ListNode
+    function mergeTwoLists(?ListNode $list1, ?ListNode $list2): ?ListNode
     {
         if (!$list2 || ($list1 && $list1->val < $list2->val)) {
-            $toSetVal = $list1->val;
-            $list1 = $list1->next;
+            $result = $list1;
+            $toWhile = $list2;
         } else {
-            $toSetVal = $list2->val;
-            $list2 = $list2->next;
+            $result = $list2;
+            $toWhile = $list1;
         }
 
-        $resultNode = new ListNode($toSetVal);
-        $prevNode = $resultNode;
-        while ($list1) {
-            if (!$list2 || $list1->val < $list2->val) {
-                $toSetVal = $list1->val;
-                $list1 = $list1->next;
-            } else {
-                $toSetVal = $list2->val;
-                $list2 = $list2->next;
+        $prevNode = $result;
+        while ($toWhile) {
+            $next = $prevNode->next;
+            if (!$next || $toWhile->val < $next->val) {
+                $toSetVal = $toWhile->val;
+                $prevNode->next = new ListNode($toWhile->val, $next);
+                $toWhile = $toWhile->next;
             }
-            $prevNode->next = new ListNode($toSetVal);
+
             $prevNode = $prevNode->next;
         }
 
-        while ($list2) {
-            $prevNode->next = new ListNode($list2->val);
-            $prevNode = $prevNode->next;
-            $list2 = $list2->next;
-        }
-
-        return $resultNode;
+        return $result;
     }
 }
 
