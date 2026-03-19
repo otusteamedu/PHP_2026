60fa54cf1e53b20079431bc214aad0c779678661
andrewizmaylov <andrew.izmaylov@gmail.com>
2026-03-19 17:19:32 +0500

    И вправду можно короче и эффективнее

--- src/LeetCode/LeetCode21.php
+++ src/LeetCode/LeetCode21.php
@@ -6,66 +6,25 @@ namespace App\LeetCode;
 
 class LeetCode21
 {
-
-    /**
-     * @param ListNode $list1
-     * @param ListNode $list2
-     * @return ListNode
-     */
     public static function mergeTwoLists(?ListNode $list1 = null, ?ListNode $list2 = null): ?ListNode
     {
-        $head = null;
+        $root = new ListNode(0);
+        $current = $root;
 
         while ($list1 !== null && $list2 !== null) {
-            $l = $list1->val;
-            $r = $list2->val;
-
-            if ($l <= $r) {
-                $node = new ListNode($l);
-                $head = self::updateHead($head, $node);
+            if ($list1->val <= $list2->val) {
+                $current->next = $list1;
                 $list1 = $list1->next;
             } else {
-                $node = new ListNode($r);
-                $head = self::updateHead($head, $node);
+                $current->next = $list2;
                 $list2 = $list2->next;
             }
-        }
-
-        // Дописываем все что осталось из первого
-        while ($list1 !== null) {
-            $node = new ListNode($list1->val);
-            $head = self::updateHead($head, $node);
-            $list1 = $list1->next;
-        }
-
-        // Дописываем все что осталось из второго
-        while ($list2 !== null) {
-            $node = new ListNode($list2->val);
-            $head = self::updateHead($head, $node);
-            $list2 = $list2->next;
-        }
-
-        return $head;
-    }
-
-    /**
-     * @param ListNode|null $head
-     * @param ListNode $node
-     * @return ListNode
-     */
-    public static function updateHead(?ListNode $head, ListNode $node): ListNode
-    {
-        if ($head === null) {
-            return $node;
-        }
-
-        $current = $head;
-        while ($current->next !== null) {
             $current = $current->next;
         }
 
-        $current->next = $node;
+        // Дописываем все что осталось от первого или второго
+        $current->next = $list1 ?? $list2;
 
-        return $head;
+        return $root->next;
     }
 }
