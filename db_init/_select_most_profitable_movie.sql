40efe2ffc7cab1afc96820b17209f1474eadb31b
Albert <haralb01@gmail.com>
2026-03-19 21:27:53 +0400

    FIX: Add seats , add status for orders improve column name seanse

--- db_init/_select_most_profitable_movie.sql
+++ db_init/_select_most_profitable_movie.sql
@@ -3,9 +3,9 @@ select * from movies where id IN (
         movie_id
     from seanses
     where id = (
-        select seans_id
+        select seanse_id
         from orders
-        group by seans_id
+        group by seanse_id
         order by sum(price) desc LIMIT 1
     )
     );
--- db_init/create_tables.sql
+++ db_init/create_tables.sql
@@ -3,10 +3,17 @@ CREATE TABLE IF NOT EXISTS rooms (
     title VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS seats (
+     id SERIAL PRIMARY KEY,
+     room_id INT NOT NULL REFERENCES rooms(id) ON DELETE CASCADE,
+     position_x INT NOT NULL,
+     position_y INT NOT NULL,
+     CONSTRAINT unique_room_positions UNIQUE (room_id, position_x, position_y)
+);
+
 CREATE TABLE IF NOT EXISTS movies (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    price NUMERIC(8,2) NOT NULL,
     duration_in_seconds INT NOT NULL
 );
 
@@ -14,15 +21,18 @@ CREATE TABLE IF NOT EXISTS seanses (
     id SERIAL PRIMARY KEY,
     room_id INT NOT NULL REFERENCES rooms(id) ON DELETE CASCADE,
     movie_id INT NOT NULL REFERENCES movies(id) ON DELETE CASCADE,
-    date DATE NOT NULL,
-    time TIME NOT NULL,
-    CONSTRAINT unique_room_datetime UNIQUE (room_id, date, time)
+    price NUMERIC(8,2) NOT NULL,
+    start_at TIMESTAMP NOT NULL,
+    CONSTRAINT unique_room_datetime UNIQUE (room_id, start_at)
 );
 
 CREATE TABLE IF NOT EXISTS orders (
     id SERIAL PRIMARY KEY,
-    seans_id INT NOT NULL REFERENCES seanses(id) ON DELETE CASCADE,
+    seanse_id INT NOT NULL REFERENCES seanses(id) ON DELETE CASCADE,
+    seat_id INT NOT NULL REFERENCES seats(id) ON DELETE CASCADE,
     price NUMERIC(8,2) NOT NULL,
+    status VARCHAR(20) DEFAULT 'pending',
     ticket_number VARCHAR(50) NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT unique_seat_per_seans UNIQUE (seanse_id, seat_id)
 );
