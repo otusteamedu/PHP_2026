f716afabeb9236d5cfbd9247cf4e3d785574a8bc
Albert <haralb01@gmail.com>
2026-04-20 00:35:22 +0400

    FIX: get by join

--- db_init/_select_most_profitable_movie.sql
+++ db_init/_select_most_profitable_movie.sql
@@ -9,3 +9,14 @@ select * from movies where id IN (
         order by sum(price) desc LIMIT 1
     )
     );
+
+-- IMPROVED
+select * from movies
+                  JOIN (
+    select movie_id from seanses
+                             JOIN (SELECT seanse_id FROM orders
+                                   WHERE paid_at >= NOW() - INTERVAL '14 days'
+                                   GROUP BY seanse_id
+                                   ORDER BY SUM(price) DESC LIMIT 1
+    ) top ON seanses.id = top.seanse_id
+) as top_movies ON top_movies.movie_id = movies.id
