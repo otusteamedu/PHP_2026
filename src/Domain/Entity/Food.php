6e386e0e6dd9fc53acbf5b6ea75bb9bd96b4afee
alex <ssokoloss1@gmail.com>
2026-05-10 22:50:42 +0300

    added observer

--- src/Domain/Entity/Food.php
+++ src/Domain/Entity/Food.php
@@ -5,14 +5,21 @@ declare(strict_types=1);
 namespace App\Domain\Entity;
 
 use App\Domain\Enum\Status;
+use SplObserver;
+use SplSubject;
 
-abstract class Food implements Cookable
+abstract class Food implements Cookable, SplSubject
 {
     public Status $status {
         get {
             return $this->status;
         }
     }
+
+    /**
+     * @var SplObserver[]
+     */
+    private array $observers = [];
 
     public function __construct(
         private string $name
@@ -28,5 +35,22 @@ abstract class Food implements Cookable
     public function cook(): void
     {
         $this->status = Status::Cooked;
+    }
+
+    public function attach(SplObserver $observer): void
+    {
+        $this->observers[spl_object_hash($observer)] = $observer;
+    }
+
+    public function detach(SplObserver $observer): void
+    {
+        unset($this->observers[spl_object_hash($observer)]);
+    }
+
+    public function notify(): void
+    {
+        foreach ($this->observers as $observer) {
+            $observer->update($this);
+        }
     }
 }
---
+++ tests/Unit/Domain/Entity/ObserverTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Unit\Domain\Entity;
+
+use App\Domain\Entity\Burger;
+use PHPUnit\Framework\TestCase;
+use SplObserver;
+
+final class ObserverTest extends TestCase
+{
+    public function testSubjectObserverFunctions(): void
+    {
+        $subject = new Burger('Cheeseburger');
+        $observer = $this->createMock(SplObserver::class);
+        $observer->expects(self::once())->method('update');
+        $subject->attach($observer);
+        $subject->notify();
+        $subject->detach($observer);
+        $subject->notify();
+    }
+}
