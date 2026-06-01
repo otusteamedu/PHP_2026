1afab8944ff4020648ba84c0dc5fd91392c4dc05
alex <ssokoloss1@gmail.com>
2026-06-01 23:12:31 +0300

    added second task solution

--- IntersectionOfTwoLinkedListsSolution.php
+++ IntersectionOfTwoLinkedListsSolution.php
@@ -27,23 +27,19 @@ declare(strict_types=1);
  *
  *
  * Example 1:
- *
- *
  * Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
  * Output: Intersected at '8'
  * Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
  * From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
  * - Note that the intersected node's value is not 1 because the nodes with value 1 in A and B (2nd node in A and 3rd node in B) are different node references. In other words, they point to two different locations in memory, while the nodes with value 8 in A and B (3rd node in A and 4th node in B) point to the same location in memory.
+ *
  * Example 2:
- *
- *
  * Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
  * Output: Intersected at '2'
  * Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
  * From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+ *
  * Example 3:
- *
- *
  * Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
  * Output: No intersection
  * Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
@@ -51,7 +47,6 @@ declare(strict_types=1);
  *
  *
  * Constraints:
- *
  * The number of nodes of listA is in the m.
  * The number of nodes of listB is in the n.
  * 1 <= m, n <= 3 * 104
@@ -66,7 +61,7 @@ class Solution
     private array $hash = [];
 
     /**
-     * Сложность O (n + m), где m это позиция искомого элемента в списке A, n - в списке B.
+     * Сложность O (n), где n это позиция самого дальнего искомого элемента.
      *
      * @param ListNode $headA
      * @param ListNode $headB
